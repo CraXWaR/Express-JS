6353a6426357e165cdde05a639d7a4bb94d9b77e
unknown <craxwar@gmail.com>
2022-10-20 12:02:05 +0300

    replacing all html with hbs views

--- bookTalk/config/database.js
+++ bookTalk/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //TODO change data base
-const CONNECTION_STRING = 'mongodb://0.0.0.0:27017/TODO... database';
+const CONNECTION_STRING = 'mongodb://0.0.0.0:27017/bookTalk';
 
 module.exports = async (app) => {
     try {
