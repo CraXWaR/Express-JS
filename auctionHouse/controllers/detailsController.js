29d9be434a71df8e2e861ab11d5bc1a998654950
unknown <craxwar@gmail.com>
2022-10-31 10:36:59 +0200

    Fixing bug so nolog user can see details

--- auctionHouse/controllers/detailsController.js
+++ auctionHouse/controllers/detailsController.js
@@ -9,21 +9,19 @@ detailsController.get('/details/:id', async (req, res) => {
     const auction = await getAuctionById(req.params.id);
     let view;
 
-    if (req.user._id) {
-        auction.isOwner = auction.owner.toString() == req.user._id.toString();
-        auction.isBid = auction.biddList.some((id) => id == req.user._id);
+    auction.isOwner = auction.owner.toString() == req.user?._id.toString();
+    auction.isBid = auction.biddList.some((id) => id == req.user?._id);
 
-        if (auction.isOwner) {
-            view = 'details-owner'
-        } else {
-            view = 'details'
-        }
-        res.render(view, {
-            title: auction.title,
-            user: req.user,
-            auction
-        });
+    if (auction.isOwner) {
+        view = 'details-owner'
+    } else {
+        view = 'details'
     }
+    res.render(view, {
+        title: auction.title,
+        user: req.user,
+        auction
+    });
 });
 
 detailsController.get('/details/:id/bid', hasUser(), async (req, res) => {
